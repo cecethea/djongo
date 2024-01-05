b6b9a96c5132c33aba6783447d96febca85072af
MARTIN CECE THEA <36125073+cecethea@users.noreply.github.com>
2024-01-05 21:54:17 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -85,7 +85,7 @@ def find_version(*file_paths):
 install_requires = [
     'sqlparse==0.4.2',
     'pymongo>=3.6.0',
-    'django>=2.1',
+    'django>=2.1',<=4.2.8
 ]
 
 if sys.version_info.major == 3 and sys.version_info.minor < 7:
